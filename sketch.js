a4f145121e44ed4a62f56c3b8218338e199469aa
Dipti-Pro <85604091+Dipti-Pro@users.noreply.github.com>
2021-06-14 10:13:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -84,5 +84,18 @@ function draw() {
 
 
 
+function detectollision(lstone,lmango){
+	
+  mangoBodyPosition=lmango.body.position
+  stoneBodyPosition=lstone.body.position
+  
+  var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
+  
+  	if(distance<=lmango.r+lstone.r)
+    {
+      
+  	  Matter.Body.setStatic(lmango.body,false);
+    }
 
+  }
 
